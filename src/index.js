b54d5539fcf059ebc3ccb82374e51e941dca352d
JhohannesK <Jdomeh77@gmail.com>
2022-06-24 01:02:45 +0000

    adding some images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const btn = document.querySelector('.btn')
-const show = document.querySelector('.show-links')
+const btn = document.querySelector('.btn');
+const hide = document.querySelector('.hide-links');
 
 btn.addEventListener('click', function () {
-    show.classList.toggle('hide-links')
-})
+	hide.classList.toggle('hide-links');
+});
+
+const ww = window.innerWidth;
+if (ww < 768) {
+	document.getElementById('ss').style.backgroundImage =
+		url('./images/bg1.jpg');
+}
+console.log(ww);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        Cairo: ["'Cairo'", 'sans-serif'],
-        dancing: ["'Dancing Script'", 'cursive'],
-        kanit: ["'Kanit'", 'sans-serif'],
-      },
-      backgroundImage: {
-        'hero': "url('./public/images/hero.jpg')"
-      },
-    },
-  },
-}
+	content: ['./src/**/*.{html, js}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Cairo: ["'Cairo'", 'sans-serif'],
+				dancing: ["'Dancing Script'", 'cursive'],
+				kanit: ["'Kanit'", 'sans-serif'],
+			},
+		},
+	},
+};
